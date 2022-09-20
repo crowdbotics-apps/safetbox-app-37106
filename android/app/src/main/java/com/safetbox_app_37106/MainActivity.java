e6e170f265ad0c07d17799ba08fe058d1facd269
Crowdbotics <team@crowdbotics.com>
2022-09-20 23:37:06 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/safetbox_app_37106/MainActivity.java
@@ -0,0 +1,15 @@
+package com.safetbox_app_37106;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "safetbox_app_37106";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "safetbox_app_37106",
+  url: "https://safetbox_app_37106.botics.co",
+  api: "https://safetbox_app_37106.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
